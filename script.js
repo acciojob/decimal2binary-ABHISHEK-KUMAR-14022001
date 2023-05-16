314623730d5ecfc3cd774c6fb2c06002a77c2368
Abhishek kumar <87716036+ABHISHEK-KUMAR-14022001@users.noreply.github.com>
2023-05-16 06:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function threeSum(arr, target) {
-// write your code here
-  
+     let binaryNum = new Array(32);
+ 
+   
+    let i = 0;
+    while (n > 0) {
+ 
+      
+        binaryNum[i] = n % 2;
+        n = Math.floor(n / 2);
+        i++;
+    }
+  binaryNum.reverse();
+	return binaryNum;
+    
 }
 
 module.exports = threeSum;
